ea444ddb43c9f6bf6586344237369cd0075f45f0
FeridBuludlu <buludluferid@gmail.com>
2024-05-21 05:27:39 +0400

    task2

---
+++ Frontend/my-app/src/components/site/Section/Section4.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Section4.css"
+const Section4 = () => {
+  return (
+    <div className='form'>
+        <h4></h4>
+        
+    </div>
+  )
+}
+
+export default Section4
